96c80bad8dcb8c757e87de6ed207e274663b1e2b
Noelle Daley <noelle@newrelic.com>
2015-10-30 14:57:34 -0700

    add article create and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,36 @@
 class ArticlesController < ApplicationController
 
+  def index
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
-  def create
-    render plain: params[:article].inspect
+  def edit
   end
+
+  def create
+    # Prevent malicious activity, only allow :title and :text
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect_to @article # redirect to article show action
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
 
 end
